e224bcaae3f3c884fda25fbae38790c9e7acdc67
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2022-03-04 02:19:19 +0100

    third time deploying in heroku adding connection string

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/app-goodtaste'; 
+const URI = 'mongodb+srv://admin:Amoamihijo2199$@cluster0.yd5np.mongodb.net/app-goodtaste?retryWrites=true&w=majority'; 
 mongoose.connect(URI)
     .then(db=> console.log('BD CONECTADA'))
     .catch(err=>console.log(err));
