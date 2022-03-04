c2aca065e6e0744a184bf3826472c1aa1318a69d
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2022-03-04 20:15:23 +0100

    intento de deploy a heroku  con cli de github

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express  = require ('express');
 const morgan = require('morgan');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 
 const { mongoose } = require('./database');
