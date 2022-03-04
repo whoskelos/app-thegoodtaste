35b9ed36c59d6b9949569a0f93934cad83a14f84
Kelvin Guerrero <kelvinguerrero2199@gmail.com>
2022-03-04 19:01:49 +0100

    fourth time deploying in heroku deleting cors line in backend

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.set('port',process.env.PORT || 3000);
 //Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors({origin:'http://localhost:4200'}));
+// app.use(cors({origin:'http://localhost:4200'}));
 
 
 //Rutas
